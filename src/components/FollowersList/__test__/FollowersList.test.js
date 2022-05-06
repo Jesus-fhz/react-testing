3b4775f622f0a660df84ed22a311506e9ef9ea50
Jesus-fhz <jesusflores9325@gmail.com>
2022-05-06 12:27:45 +1000

    Jest setup and teardown, beforeEach, beforeAll, afterEach, AfterAll

--- src/components/FollowersList/__test__/FollowersList.test.js
+++ src/components/FollowersList/__test__/FollowersList.test.js
@@ -9,17 +9,39 @@ const MockFollwersList = () => {
         </BrowserRouter>
     )
 }
-
+const currentComponent = () => render(<MockFollwersList/>);
 describe("FollowersList component - tests",() => {
+
+    let userFirstElement;
+    let userAllElements;
+    
+    //BeforeEach It will execute each time before each test
+    beforeEach(async () =>  {
+        currentComponent();
+        userFirstElement =  await screen.findByTestId('user-item-0');
+        userAllElements =  await screen.findAllByTestId(/user-item/i);
+    })
+
+    //BeforeAll will execute one time before all test are run.
+    beforeAll(()=>{
+        console.log("I run first, before all the tests run")
+    })
+
+    //afterEach it will run after each test has been run
+    afterEach(()=>{
+        console.log("I run after every test has been run")
+    })
+
+    //After all will run after each test has been run.
+    afterAll(()=>{
+        console.log("I will run once all test has been finished")
+    })
+
     it("Should render the first user", async  ()=>{
-        render(<MockFollwersList/>)
-        const userFirstElement =  await screen.findByTestId('user-item-0');
         expect(userFirstElement).toBeInTheDocument();
     })
 
     it("Shoul render all users,(at least 5)", async  ()=>{
-        render(<MockFollwersList/>)
-        const userAllElements =  await screen.findAllByTestId(/user-item/i);
         expect(userAllElements.length).toBeGreaterThanOrEqual(5);
     })
 })
