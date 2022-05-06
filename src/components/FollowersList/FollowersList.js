c7d35665461fde8e50198a08c3402ee789905eeb
Jesus-fhz <jesusflores9325@gmail.com>
2022-05-06 10:57:54 +1000

    working with async requests, testing

--- src/components/FollowersList/FollowersList.js
+++ src/components/FollowersList/FollowersList.js
@@ -19,9 +19,9 @@ export default function FollowersList() {
     return (
         <div className="followerslist-container">
             <div>
-                {followers.map(follower => (
-                    <div className="follower-item">
-                        <img src={follower.picture.large}/>
+                {followers.map((follower,index) => (
+                    <div className="follower-item" data-testid={`user-item-${index}`} key={index}>
+                        <img src={follower.picture.large} alt=""/>
                         <div className="followers-details">
                             <div className="follower-item-name">
                                 <h4>{follower.name.first}</h4> <h4>{follower.name.last}</h4>
---
+++ src/components/FollowersList/__test__/FollowersList.test.js
@@ -0,0 +1,27 @@
+import {render, screen, fireEvent} from "@testing-library/react"
+import userEvent from '@testing-library/user-event';
+import { BrowserRouter } from "react-router-dom"
+import FollowersList from "../FollowersList"
+
+const MockFollwersList = () => {
+    return (
+        <BrowserRouter>
+            <FollowersList/>
+        </BrowserRouter>
+    )
+}
+ 
+describe("FollowersList component - tests",() => {
+    it("Should render the first user", async  ()=>{
+        render(<MockFollwersList/>)
+        const userFirstElement =  await screen.findByTestId('user-item-0');
+        expect(userFirstElement).toBeInTheDocument();
+    })
+
+    it("Shoul render all users,(at least 5)", async  ()=>{
+        render(<MockFollwersList/>)
+        const userAllElements =  await screen.findAllByTestId(/user-item/i);
+        expect(userAllElements.length).toBeGreaterThanOrEqual(5);
+    })
+})
+
